8107575e064914f0f760f599020b6ea77d6403ed
Elteras <b.m.schwartz92@googlemail.com>
2022-07-19 15:28:50 +0200

    Building app structure
    Task 2.4 - added some data on movies, further back-end logic, etc.

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express'),
+    morgan = require('morgan');
+const app = express();
+
+
+let topMovies = [
+    {
+        title: 'Lock, Stock, and Two Smoking Barrels',
+        director: 'Guy Ritchie'
+    },
+    {
+        title: 'WALL-E',
+        director: 'Andrew Stanton'
+    },
+    {
+        title: 'The Raid',
+        director: 'Gareth Evans'
+    },
+    {
+        title: 'Kick-Ass',
+        director: 'Matthew Vaughn & Jeff Wadlow'        
+    },
+    {
+        title: 'The Grand Budapest Hotel',
+        director: 'Wes Anderson'        
+    },
+    {
+        title: 'Pulp Fiction',
+        director: 'Quentin Tarantino'        
+    },
+    {
+        title: 'The Hateful Eight',
+        director: 'Quentin Tarantino'        
+    },
+    {
+        title: 'The Lives of Others',
+        director: 'Florian Henckel von Donnersmarck'   
+    },
+    {
+        title: 'Everything Everywhere All At Once',
+        director: 'Daniel Kwan & Daniel Scheinert'        
+    },
+    {
+        title: 'The Cabin in the Woods',
+        director: 'Drew Goddard'        
+    },
+
+  ];
+
+
+  app.use(morgan('common'));
+
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to my film app!');
+  });  
+
+  app.get('/movies', (req, res) => {
+    res.json(topMovies);
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
+
+  app.use('/documentation.html', express.static('public'));
