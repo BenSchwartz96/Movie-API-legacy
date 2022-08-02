c19e2b95ca00a91ae94b69010114d16fbced2189
Elteras <b.m.schwartz92@googlemail.com>
2022-08-02 02:07:50 +0200

    Set up routes and added data

--- index.js
+++ index.js
@@ -47,27 +47,83 @@ let topMovies = [
 
   ];
 
-
-  app.use(morgan('common'));
-
-
-  app.get('/', (req, res) => {
-    res.send('Welcome to my film app!');
-  });  
-
-  app.get('/movies', (req, res) => {
-    res.json(topMovies);
-  });
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+  let directors = [
+    {
+        name: 'Guy Ritchie',
+        bio: 'Blank',
+        dob: '1968'        
+    },
+    {
+        name: 'Andrew Stanton',
+        bio: 'Also blank',
+        dob: '1965'
+    }
+    
+  ]
 
 
-  app.use('/', express.static('public'));
+    app.use(morgan('common'));
 
 
-  app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
-  });
+    app.get('/', (req, res) => {
+        res.send('Welcome to my film app!');
+    });  
+
+     app.get('/movies', (req, res) => {
+        res.json(topMovies);
+    });
+
+    app.get('/movies/directors', (req, res) => {
+      res.json(directors);
+    });
+
+    app.get('/movies/directors/:name', (req, res) => {
+        res.json(directors.find((director) => 
+        {return director.name === req.params.name}));
+    });
+  
+    app.get('/movies/:title', (req, res) => {
+      res.json(topMovies.find((movie) => 
+      {return movie.title === req.params.title}));
+    });
+    
+    // app.get('/movies/:genre', (req, res) => {
+    //     res.json(topMovies.find((movie) => 
+    //     {return movie.title === req.params.title}));
+    // });//incomplete
+    
+
+    app.post('/users', (req, res) => {
+        res.send('successful post request');
+    });
+
+    app.put('/users/:name', (req, res) => {
+    //code hre
+    });
+
+    app.post('/users/:name/favorites', (req, res) => {
+    //code here
+    });
+  
+    app.delete('/users/:name/favorites', (req, res) => {
+    //code here
+    });
+
+    app.delete('/users/:name', (req, res) => {
+    //code here
+    });
+
+
+
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something broke!');
+    });
+
+
+    app.use('/', express.static('public'));
+
+
+    app.listen(8080, () => {
+        console.log('Your app is listening on port 8080.');
+    });
