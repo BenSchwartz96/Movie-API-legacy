80404c1f4f5aaddc797941edd57f25233ef277f5
Elteras <b.m.schwartz92@googlemail.com>
2022-08-04 15:18:08 +0200

    Changes to routes/formatting
    For task 2.5. Added a response message to further routes, changed documentation, and formatted the code.

--- index.js
+++ index.js
@@ -18,95 +18,95 @@ let topMovies = [
     },
     {
         title: 'Kick-Ass',
-        director: 'Matthew Vaughn & Jeff Wadlow'        
+        director: 'Matthew Vaughn & Jeff Wadlow'
     },
     {
         title: 'The Grand Budapest Hotel',
-        director: 'Wes Anderson'        
+        director: 'Wes Anderson'
     },
     {
         title: 'Pulp Fiction',
-        director: 'Quentin Tarantino'        
+        director: 'Quentin Tarantino'
     },
     {
         title: 'The Hateful Eight',
-        director: 'Quentin Tarantino'        
+        director: 'Quentin Tarantino'
     },
     {
         title: 'The Lives of Others',
-        director: 'Florian Henckel von Donnersmarck'   
+        director: 'Florian Henckel von Donnersmarck'
     },
     {
         title: 'Everything Everywhere All At Once',
-        director: 'Daniel Kwan & Daniel Scheinert'        
+        director: 'Daniel Kwan & Daniel Scheinert'
     },
     {
         title: 'The Cabin in the Woods',
-        director: 'Drew Goddard'        
+        director: 'Drew Goddard'
     },
 
-  ];
+];
 
 
 
-    app.use(morgan('common'));
+app.use(morgan('common'));
 
 
-    app.get('/', (req, res) => {
-        res.send('Welcome to my film app!');
-    });  
+app.get('/', (req, res) => {
+    res.send('Welcome to my film app!');
+});
 
-     app.get('/movies', (req, res) => {
-        res.json(topMovies);
-    });
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
 
 
-    app.get('/movies/directors/:name', (req, res) => {
-        res.send('successful get request');
-    });
-  
-    app.get('/movies/:title', (req, res) => {
+app.get('/movies/directors/:name', (req, res) => {
+    res.send('successful get request');
+});
+
+app.get('/movies/:title', (req, res) => {
     //   res.json(topMovies.find((movie) => 
     //   {return movie.title === req.params.title}));
     res.send('successful get request');
-    });
-    
-    app.get('/movies/genres/:genre', (req, res) => {
-        res.send('successful get request');
-    });
-    
+});
 
-    app.post('/users', (req, res) => {
-        res.send('successful post request');
-    });
-
-    app.put('/users/:name', (req, res) => {
-        res.send('successful put request');
-    });
-
-    app.post('/users/:name/favorites/:movieID', (req, res) => {
-    //code here
-    });
-  
-    app.delete('/users/:name/favorites/:movieID', (req, res) => {
-    //code here
-    });
-
-    app.delete('/users/:name', (req, res) => {
-    //code here
-    });
+app.get('/movies/genres/:genre', (req, res) => {
+    res.send('successful get request');
+});
 
 
+app.post('/users', (req, res) => {
+    res.send('successful post request');
+});
 
-    app.use((err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).send('Something broke!');
-    });
+app.put('/users/:name', (req, res) => {
+    res.send('successful put request');
+});
+
+app.post('/users/:name/favorites/:movieID', (req, res) => {
+    res.send('successful post request');
+});
+
+app.delete('/users/:name/favorites/:movieID', (req, res) => {
+    res.send('successful delete request');
+});
+
+app.delete('/users/:name', (req, res) => {
+    res.send('successful delete request');
+});
 
 
-    app.use('/', express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 
-    app.listen(8080, () => {
-        console.log('Your app is listening on port 8080.');
-    });
+app.use('/', express.static('public'));
+
+
+app.listen(8080, () => {
+    console.log('Your app is listening on port 8080.');
+});
