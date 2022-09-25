8e3a0b2fd89d40392eecf3c2b4dd345e63594708
Elteras <b.m.schwartz92@googlemail.com>
2022-09-25 14:27:57 +0200

    Task 3.4

--- index.js
+++ index.js
@@ -52,17 +52,29 @@ app.get('/', (req, res) => {
 
 
 //For finding all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then((movies) => {
-        res.status(201).json(movies);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send('Error: ' + err);
-      });
-  });
+// app.get('/movies', passport.authenticate('jwt', { session: false }),
+//   (req, res) => {
+//     Movies.find()
+//       .then((movies) => {
+//         res.status(201).json(movies);
+//       })
+//       .catch((err) => {
+//         console.error(err);
+//         res.status(500).send('Error: ' + err);
+//       });
+//   });
+
+//temp code for 3.4
+app.get("/movies", function (req, res) {
+  Movies.find()
+    .then(function (movies) {
+      res.status(201).json(movies);
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
+});
 
 //For finding a director by name                
 app.get('/movies/directors/:director', passport.authenticate('jwt', { session: false }),
